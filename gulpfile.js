953fd79f64d80da2b77fccc3a777975286442762
dyveto <devegatorresdylan@gmail.com>
2025-06-16 01:29:32 -0500

    otra opcion

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,38 @@
-// import {src, dest, watch, series} from 'gulp'
-// import * as dartSass from 'sass'
-// import gulpSass from 'gulp-sass'
+import {src, dest, watch, series} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
 
-// const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass)
 
-// export function js( done ){
-//     src('src/js/app.js')
-//         .pipe( dest('build/js'))
-
-//     done()
-// }
-
-// export function css( done ) {
-//     src('src/scss/app.scss', {sourcemaps: true})
-//         .pipe( sass().on('error', sass.logError) )
-//         .pipe( dest('build/css', {sourcemaps: true}))
-
-//     done()
-// }
- 
-// export function dev() {
-//     watch('src/scss/**/*.scss', css)
-//     watch('src/js/**/*.js', js)
-// }
-
-// // Definir la tarea "build"
-// export const build = series(js, css);
-
-// // Tarea por defecto
-// export default series(js, css, dev);
-
-import { src, dest, watch, series } from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-
-const sass = gulpSass(dartSass);
-
-// Mover archivos HTML a dist
 export function html(done) {
     src('*.html')
-        .pipe(dest('dist'));
+        .pipe(dest('build'));
     done();
 }
 
-// Compilar JS
-export function js(done) {
+
+export function js( done ){
     src('src/js/app.js')
-        .pipe(dest('dist/js'));
-    done();
+        .pipe( dest('build/js'))
+
+    done()
 }
 
-// Compilar SCSS
-export function css(done) {
-    src('src/scss/app.scss', { sourcemaps: true })
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('dist/css', { sourcemaps: '.' }));
-    done();
-}
+export function css( done ) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: true}))
 
-// Dev
+    done()
+}
+ 
 export function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('src/js/**/*.js', js);
-    watch('./*.html', html);
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
 
-// Build
-export const build = series(html, js, css);
-export default series(html, js, css, dev);
+// Definir la tarea "build"
+export const build = series(js, css);
+
+// Tarea por defecto
+export default series(js, css, dev);
